9b83f2f973b13bd993226b1a758db654a6d79b11
Gerren <g.ballard23@outlook.com>
2021-12-31 12:58:28 -0600

    Done

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i = 0; i < word.length / 2; i++){
+    
+    const j = word.length - 1 - i
+    const startChar = word[i]
+    const endChar = word[j]
+
+    if(startChar !== endChar) return false;
+
+  }
+
+  return true
 }
 
 /* 
   Add your pseudocode here
+  iterate through have of the input string
+  compare first letter input string to the last letter of input string
+  if the letters do not match return false
 */
 
 /*
   Add written explanation of your solution here
+  functon takes first half of the word and compares it to the second half
+  it returns true if they match and false if they do not 
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +32,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expexting: true");
+  console.log("=>", isPalindrome('tacocat'));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
